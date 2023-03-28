b3dec1748c7521e35aa81c855a89381a719d798d
tufaelkhan <tufaelkhan247@gmail.com>
2023-03-27 22:10:04 -0700

    complete

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,9 +6,15 @@ const Card = ({cart}) => {
     console.log(cart)
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(let product of cart){
-        totalPrice = totalPrice + product.price;
+        // if(product.quantity === 0){
+        //     product.quantity =1;
+        // }
+        // // product.quantity = product.quantity || 1;
+        totalPrice = totalPrice + product.price * product.quantity;
         totalShipping = totalShipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = totalPrice*7/100;
     const grandTotal = totalPrice + totalShipping + tax
@@ -16,7 +22,8 @@ const Card = ({cart}) => {
     return (
         <div className='cart'>
             <h3>order summary</h3>
-            <p>Selected Items: {cart.length}</p>
+            {/* <p>Selected Items: {cart.length}</p> */}
+            <p>Selected Items: ${quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/components/Shop/Shorp.jsx
+++ src/components/Shop/Shorp.jsx
@@ -35,9 +35,21 @@ const Shorp = () => {
 
     const hadleAddCart = (product) =>{
         // cart.push(product)
-        const newCart = [...cart, product]
-        setCart(newCart)
+        let newCart = [];
+        // const newCart = [...cart, product]
+        // if product doesn't exist in the cart, then set quantity = 1;
+        // if exists update the quantity by 1
+        const exists = cart.find(pd => pd.id ===  product.id);
+        if(!exists){
+            product.quantity = 1;
+            newCart = [...cart, product];
+        }else{
+            exists.quantity = exists.quantity + 1;
+            const reamining = cart.filter(pd => pd.id !== product.id);
+            newCart = [...reamining, exists];
+        }
 
+        setCart(newCart)
         addToDb(product.id)
     }
     return (
